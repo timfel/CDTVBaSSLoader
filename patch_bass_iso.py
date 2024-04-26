f5c1e1a2fcfbe2e4e082113e9ad9552fda84fe51
Tim Felgentreff <timfelgentreff@gmail.com>
2024-04-26 14:21:23 +0200

    ok, just fall to cli

--- patch_bass_iso.py
+++ patch_bass_iso.py
@@ -1,51 +1,19 @@
-import pycdlib
-import os.path
-
 from io import BytesIO
 from pathlib import Path
 from tkinter import filedialog
 
 
-LOADER_NAME = "CDTVBaSS"
-LOADER_BYTES = LOADER_NAME.encode("ascii")
-ORIGINAL_BYTES = b"steelsky"
-assert len(LOADER_BYTES) == len(ORIGINAL_BYTES)
-
-
 if __name__ == "__main__":
+    old_value = b"steelsky\n\n"
+    new_value = b"echo    \n\n"
+    assert len(new_value) == len(old_value), len(new_value)
     isoname = filedialog.askopenfilename(
         title="Choose Beneath a Steel Sky CD image",
         filetypes=[("ISO files", "*.iso")]
     )
     with open(isoname, 'rb') as f:
         contents = f.read()
-
-    iso = pycdlib.PyCdlib()
-    iso.open_fp(BytesIO(contents))
-
-    # change the startup sequence to run the loader instead
-    startup_sequence = BytesIO()
-    iso.get_file_from_iso_fp(startup_sequence, iso_path='/s/startup-sequence;1')
-    new_value = startup_sequence.getvalue().replace(ORIGINAL_BYTES, LOADER_BYTES)
-    print(new_value)
-    startup_sequence = BytesIO(new_value)
-    iso.modify_file_in_place(
-        fp=startup_sequence,
-        length=len(new_value),
-        iso_path='/s/startup-sequence;1',
-    )
-
-    # add uae/dh0/BaSSLoader to the ISO
-    with open(os.path.join(os.path.dirname(__file__), "uae", "dh0", "BaSSLoader"), "rb") as f:
-        loader = f.read()
-    iso.add_fp(
-        fp=BytesIO(loader),
-        length=len(loader),
-        iso_path=(b"/" + LOADER_BYTES + b";1").decode("ascii"),
-    )
-    iso.force_consistency()
     patched_name = Path(isoname).with_name(f"{Path(isoname).stem}-patched{Path(isoname).suffix}")
     print("Saving patched ISO", patched_name)
-
-    iso.write(patched_name)
-    iso.close()
+    with open(patched_name, 'wb') as f:
+        f.write(contents.replace(old_value, new_value))
