72d5e5e726fa2365cf305960450683e90c3854fe
Tim Felgentreff <timfelgentreff@gmail.com>
2024-04-26 14:30:52 +0200

    make the patch trivial

---
+++ README.md
@@ -0,0 +1,4 @@
+A trivial script to make the Beneath a Steel Sky CD32 image launch correctly on a CDTV.
+The CDTV will still need 2MB of Chip RAM, KS 3.1 or newer, and the CDTV 2.35 Extended ROMs
+from CDTV Land, because the BaSS CD32 disk does not have the CDTV.TM file embedded
+that old extended ROMs check for.
--- patch_bass_iso.py
+++ patch_bass_iso.py
@@ -1,19 +1,21 @@
-from io import BytesIO
 from pathlib import Path
 from tkinter import filedialog
 
 
 if __name__ == "__main__":
     old_value = b"steelsky\n\n"
-    new_value = b"echo    \n\n"
+    new_value = b"newcli\nss\n"
     assert len(new_value) == len(old_value), len(new_value)
+    old_name = b"SteelSky;1"
+    new_name = b"ss;1" + b"\0" * (len(old_name) - len(b"ss;1"))
+    assert len(new_name) == len(old_name), len(new_name)
     isoname = filedialog.askopenfilename(
-        title="Choose Beneath a Steel Sky CD image",
-        filetypes=[("ISO files", "*.iso")]
+        title="Choose Beneath a Steel Sky CD iso or bin",
+        filetypes=[("ISO files", "*.iso"), ("BIN files", "*.bin")],
     )
     with open(isoname, 'rb') as f:
         contents = f.read()
     patched_name = Path(isoname).with_name(f"{Path(isoname).stem}-patched{Path(isoname).suffix}")
-    print("Saving patched ISO", patched_name)
+    print("Saving patched CD image", patched_name)
     with open(patched_name, 'wb') as f:
-        f.write(contents.replace(old_value, new_value))
+        f.write(contents.replace(old_value, new_value).replace(old_name, new_name))
